f08afffd1cd93a7c9abe7cfcbe243b0b23a71b65
venya <mymyhighlight@gmail.com>
2017-07-24 19:47:58 +0300

    init

---
+++ src/main/java/com/test/servlet3_0/MyServlet.java
@@ -0,0 +1,31 @@
+package com.test.servlet3_0;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by venya on 24.07.17.
+ */
+@WebServlet("/s")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("Hello Word");
+    }
+}
