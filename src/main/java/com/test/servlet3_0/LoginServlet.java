9045134533d4419bad81be1824b835f65cf36378
venya <mymyhighlight@gmail.com>
2017-07-26 17:48:48 +0300

    Added login and checkout features

--- src/main/java/com/test/servlet3_0/LoginServlet.java
+++ src/main/java/com/test/servlet3_0/LoginServlet.java
@@ -7,9 +7,7 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 
 /**
  * Servlet Tutorial - Servlet Example
@@ -48,6 +46,13 @@ public class LoginServlet extends HttpServlet {
         log("User="+user+"::password="+pwd);
 
         if(userID.equals(user) && password.equals(pwd)){
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "Pankaj");
+            //setting session to expiry in 30 mins
+            session.setMaxInactiveInterval(30*60);
+            Cookie userName = new Cookie("user", user);
+            userName.setMaxAge(30*60);
+            response.addCookie(userName);
             response.sendRedirect("LoginSuccess.jsp");
         }else{
             RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
