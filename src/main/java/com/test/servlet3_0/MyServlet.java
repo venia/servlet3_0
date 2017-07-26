497277f19d0e76dc5c4f2252574288778facd279
venya <mymyhighlight@gmail.com>
2017-07-26 14:56:45 +0300

    Refactored .gitignore

--- src/main/java/com/test/servlet3_0/MyServlet.java
+++ src/main/java/com/test/servlet3_0/MyServlet.java
@@ -1,5 +1,7 @@
 package com.test.servlet3_0;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +14,12 @@ import java.io.PrintWriter;
  * Created by venya on 24.07.17.
  */
 @WebServlet("/s")
+@Slf4j
 public class MyServlet extends HttpServlet {
+    public MyServlet() {
+        super();
+        log("Registered: [MyServlet]");
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
